78ba264df0bae9b4b806ab59eb82bbd9ce36f969
Roberto Alcazar <ralcazar@gmail.com>
2014-09-04 18:47:59 +0200

    Changed app key

--- tutorial.js
+++ tutorial.js
@@ -1,5 +1,5 @@
 // Insert your Dropbox app key here:
-var DROPBOX_APP_KEY = 'xoit9j3uwj9vmdv';
+var DROPBOX_APP_KEY = 'sufwramlfghovn9';
 
 // Exposed for easy access in the browser console.
 var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
